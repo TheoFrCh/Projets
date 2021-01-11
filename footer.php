2b68b8778fb2949c434a2c1ad59042f4dbd6dba4
TheoFrCh <theo.chiron26@gmail.com>
2021-01-11 10:10:25 +0100

    ajout du footer et du header en php pour eviter les répetitions

---
+++ footer.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+
+<footer>
+    <h3>Contact</h3>
+    <p><a href="mailto:theo.chiron@le-campus-numerique.fr"><img src="Enveloppe.png" height="50" width="50"
+                                                                alt="Me contacter par Mail" title="Me contacter par Mail"></a>
+
+    </p>
+
+</footer>
+
+
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CV Théo</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+
+<header>
+    <div class="nom">
+        <p>
+            CHIRON THÉO
+        </p>
+    </div>
+
+
+    <img src="photo_cv.jpg" alt="photo Théo cv" height="160" width="100" title="Photo CV">
+
+
+</header>
+
+
+<nav>
+
+    <p><a href="index.html">CV</a></p>
+    <p><a href="Hobbies.html">Hobbies</a></p>
+    <p><a href="contact.html">Contact</a></p>
+
+</nav>
+</body>
+</html>
+
