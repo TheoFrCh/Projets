2e619bc18c1964458579be1a7a516237ce8bdff6
TheoFrCh <theo.chiron26@gmail.com>
2021-01-12 11:49:06 +0100

    Antislash pour retrouver enveloppe.png en distant

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer>
     <h3>Contact</h3>
-    <p><a href="mailto:theo.chiron@le-campus-numerique.fr"><img src="Enveloppe.png" height="50" width="50"
+    <p><a href="mailto:theo.chiron@le-campus-numerique.fr"><img src="\Enveloppe.png" height="50" width="50"
                                                                 alt="Me contacter par Mail" title="Me contacter par Mail"></a>
 
     </p>
