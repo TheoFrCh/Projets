3d81f8b9024399e3bb335d28aaf526e79613859d
TheoFrCh <theo.chiron26@gmail.com>
2021-01-12 11:51:49 +0100

    Je transforme mon index.php en font controller

--- index.php
+++ index.php
@@ -1,8 +1,29 @@
 <?php
 $metaDescription = "Voici le site servant de support à mon CV";
 $metaTitle = "CV Théo";
-require 'IndexCV.php';
-echo "Hello world !";
+require 'header.php';
+
+
+if (isset($_GET['page'])) {
+    $page = $_GET['page'];
+
+
+    if ($page == 'acceuil') {
+        require 'IndexCV.php';
+    }
+
+    if ($page == 'hobbies') {
+        require 'hobbies.php';
+    }
+
+    if ($page == 'contact') {
+        require 'contact.php';
+    }
+    if ($page !='contact'& $page !='hobbies'& $page!='acceuil'){
+        echo "Erreur 404";
+    }
+}
+require 'footer.php';
 ?>
 
 <br>
