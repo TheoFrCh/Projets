c801569eb85b9d3d577f15767fe932c2bfd63ac0
TheoFrCh <theo.chiron26@gmail.com>
2021-01-12 11:48:01 +0100

    Anti slash pour pouvoir retrouver mes images dans mon site et changement des href pour que mon site ne soit plus en local

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?=$metaTitle?></title>
     <meta name="description" content="<?=$metaDescription?>">
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="/style.css">
 </head>
 
 <body>
@@ -19,7 +19,7 @@
     </div>
 
 
-    <img src="photo_cv.jpg" alt="photo Théo cv" height="160" width="100" title="Photo CV">
+    <img src="\photo_cv.jpg" alt="photo Théo cv" height="160" width="100" title="Photo CV">
 
 
 </header>
@@ -27,9 +27,9 @@
 
 <nav>
 
-    <p><a href="IndexCV.php">CV</a></p>
-    <p><a href="hobbies.php">Hobbies</a></p>
-    <p><a href="contact.php">Contact</a></p>
+    <p><a href="http://projets/index.php/?page=acceuil">CV</a></p>
+    <p><a href="http://projets/index.php/?page=hobbies">Hobbies</a></p>
+    <p><a href="http://projets/index.php/?page=contact">Contact</a></p>
 
 </nav>
 
